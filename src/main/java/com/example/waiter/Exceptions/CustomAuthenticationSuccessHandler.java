68f569f0f38e4b001d87e0f9c9395f8ab518f5b0
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-02-20 17:36:54 +0200

    Authentication auth for waiter

--- src/main/java/com/example/waiter/Exceptions/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/waiter/Exceptions/CustomAuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package com.example.waiter.Exceptions;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;

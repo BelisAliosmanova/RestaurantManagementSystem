4e3a66c12bff0be0670677545ff5a145217ce8a1
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-02-20 21:29:34 +0200

    Restaurant menu - edit and delete from menu

--- src/main/java/com/example/waiter/Security/WebSecurityConfigStaff.java
+++ src/main/java/com/example/waiter/Security/WebSecurityConfigStaff.java
@@ -50,7 +50,7 @@ public class WebSecurityConfigStaff extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .mvcMatchers("/homePageCook")
                 .hasAnyAuthority("COOK")
-                .mvcMatchers("/homePageWaiter", "/addDish", "/addDrink")
+                .mvcMatchers("/homePageWaiter", "/addDish", "/addDrink", "/restaurantMenu")
                 .hasAnyAuthority("WAITER")
                 .and()
                 .formLogin().loginPage("/login").permitAll()

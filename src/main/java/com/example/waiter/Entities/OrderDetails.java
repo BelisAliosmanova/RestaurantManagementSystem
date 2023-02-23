b0b442403b77cdb86102c59a1657b6564a3e7fb9
firdesrr <firdesrr@abv.bg>
2023-02-23 14:46:31 +0200

    order_dish and order_drink - entities for bd

--- src/main/java/com/example/waiter/Entities/OrderDetails.java
+++ src/main/java/com/example/waiter/Entities/OrderDetails.java
@@ -1,53 +1,53 @@
-package com.example.waiter.Entities;
-
-import javax.persistence.*;
-import java.util.Optional;
-
-@Entity
-public class OrderDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Long orderId;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "dish_id")
-    private Long dishId;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "drink_id")
-    private Long drinkId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Long getDishId() {
-        return dishId;
-    }
-
-    public void setDishId(Long dishId) {
-        this.dishId = dishId;
-    }
-
-    public Long getDrinkId() {
-        return drinkId;
-    }
-
-    public void setDrinkId(Long drinkId) {
-        this.drinkId = drinkId;
-    }
-}
+//package com.example.waiter.Entities;
+//
+//import javax.persistence.*;
+//import java.util.Optional;
+//
+//@Entity
+//public class OrderDetails {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
+//    private Long id;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "order_id")
+//    private Long orderId;
+//    @OneToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "dish_id")
+//    private Long dishId;
+//    @OneToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "drink_id")
+//    private Long drinkId;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getOrderId() {
+//        return orderId;
+//    }
+//
+//    public void setOrderId(Long orderId) {
+//        this.orderId = orderId;
+//    }
+//
+//    public Long getDishId() {
+//        return dishId;
+//    }
+//
+//    public void setDishId(Long dishId) {
+//        this.dishId = dishId;
+//    }
+//
+//    public Long getDrinkId() {
+//        return drinkId;
+//    }
+//
+//    public void setDrinkId(Long drinkId) {
+//        this.drinkId = drinkId;
+//    }
+//}

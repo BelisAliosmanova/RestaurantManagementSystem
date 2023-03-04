92ec14f59f722d3053988a5e348317eff3daa712
firdesrr <firdesrr@abv.bg>
2023-03-04 15:30:17 +0200

    TestLoadUserByUserName

---
+++ src/test/java/com/example/waiter/StaffDetailsServiceTest.java
@@ -0,0 +1,49 @@
+package com.example.waiter;
+
+import com.example.waiter.Entities.Staff;
+import com.example.waiter.Repositories.StaffRepository;
+import com.example.waiter.Services.StaffDetailsServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class StaffDetailsServiceTest {
+    @Mock
+    StaffRepository staffRepository;
+    @InjectMocks
+    StaffDetailsServiceImpl staffDetailsServiceImpl;
+
+    @Test
+    public void TestLoadUserByUserName() {
+        Staff staff = new Staff();
+        staff.setUsername("belis");
+        staff.setPassword("b1234");
+        staff.setEnabled(true);
+        when(staffRepository.getStaffByUsername("belis")).thenReturn(staff);
+        UserDetails userDetails = staffDetailsServiceImpl.loadUserByUsername("belis");
+        assertEquals(staff.getUsername(), userDetails.getUsername());
+        assertEquals(staff.getPassword(), userDetails.getPassword());
+        assertEquals(staff.isEnabled(), userDetails.isEnabled());
+    }
+
+    @Test
+    public void testLoadUserByUsername_InvalidUsername() {
+        when(staffRepository.getStaffByUsername("johndoe")).thenReturn(null);
+        Exception exception = assertThrows(
+                UsernameNotFoundException.class,
+                () -> staffDetailsServiceImpl.loadUserByUsername("johndoe")
+        );
+        String expectedMessage = "Could not find user";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
--- src/test/java/com/example/waiter/StaffServiceTest.java
+++ src/test/java/com/example/waiter/StaffServiceTest.java
@@ -1,23 +1,34 @@
 package com.example.waiter;
 
+import com.example.waiter.Entities.Order;
 import com.example.waiter.Entities.Staff;
 import com.example.waiter.Repositories.OrderDishRepository;
 import com.example.waiter.Repositories.OrderRepository;
 import com.example.waiter.Repositories.StaffRepository;
 import com.example.waiter.Services.StaffService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StaffServiceTest {
@@ -27,18 +38,21 @@ public class StaffServiceTest {
     StaffRepository staffRepository;
     @Mock
     OrderRepository orderRepository;
+
     @Test
     void register() {
         Model model = new ExtendedModelMap();
         String viewName = staffService.showRegistrationForm(model);
         assertEquals("/register", viewName);
     }
+
     @Test
     void cookReference() {
         Model model = new ExtendedModelMap();
         String viewName = staffService.cookReference(model);
         assertEquals("/cookReference", viewName);
     }
+
     @Test
     public void homePageWaiterShouldAddUsernameToModel() throws Exception {
         Authentication authentication = new TestingAuthenticationToken("user", "password");
@@ -48,6 +62,7 @@ public class StaffServiceTest {
         String viewName = new StaffService().homePageWaiter(model);
         assertEquals("/homePageWaiter", viewName);
     }
+
     @Test
     public void homePageCookShouldAddUsernameToModel() throws Exception {
         Authentication authentication = new TestingAuthenticationToken("user", "password");
@@ -58,4 +73,6 @@ public class StaffServiceTest {
         assertEquals("/homePageCook", viewName);
     }
 
+
 }
+

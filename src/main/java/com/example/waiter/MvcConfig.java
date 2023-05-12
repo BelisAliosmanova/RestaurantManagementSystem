0eb57bcb1ff8c4701064554dccc736c09a3be86b
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-05-12 22:10:34 +0300

    hosting.....

--- src/main/java/com/example/waiter/MvcConfig.java
+++ src/main/java/com/example/waiter/MvcConfig.java
@@ -43,8 +43,8 @@ public class MvcConfig  implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 
 }

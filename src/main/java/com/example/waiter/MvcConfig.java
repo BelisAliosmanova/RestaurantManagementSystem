a157151571ec53e76bd13c92163f1f1914edbd77
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-05-12 21:28:57 +0300

    hosting.....

---
+++ src/main/java/com/example/waiter/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.waiter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+
+}

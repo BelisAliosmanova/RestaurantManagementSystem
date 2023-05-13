dfabc1664a46424c481ca4b091e5be7b00956920
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-05-13 20:17:19 +0300

    hosting.....

--- src/main/java/com/example/waiter/MvcConfig.java
+++ src/main/java/com/example/waiter/MvcConfig.java
@@ -4,66 +4,61 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig  implements WebMvcConfigurer {
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
-        return templateEngine;
-    }
-
 //    @Bean
-//    public ThymeleafViewResolver viewResolver() {
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+//        return templateEngine;
+//    }
+//
+////    @Bean
+////    public ThymeleafViewResolver viewResolver() {
+////        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+////        viewResolver.setTemplateEngine(templateEngine());
+////        return viewResolver;
+////    }
+//
+//    private ClassLoaderTemplateResolver thymeleafTemplateResolver() {
+//        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+//        templateResolver.setPrefix("templates/");
+//        templateResolver.setSuffix(".html");
+//        templateResolver.setTemplateMode("HTML");
+//        templateResolver.setCharacterEncoding("UTF-8");
+//        return templateResolver;
+//    }
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//        registry.jsp("/WEB-INF/views/", ".jsp");
+//    }
+//
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**")
+//                .addResourceLocations("/resources/")
+//                .addResourceLocations("/resources/static/")
+//                .addResourceLocations("/resources/static/css/")
+//                .addResourceLocations("/resources/static/pictures/")
+//                .addResourceLocations("/resources/templates/")
+//                .addResourceLocations("/resources/templates/home/");
+//    }
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+//        templateResolver.setTemplateMode("XHTML");
+//        templateResolver.setPrefix("views/");
+//        templateResolver.setSuffix(".html");
+//
+//        SpringTemplateEngine engine = new SpringTemplateEngine();
+//        engine.setTemplateResolver(templateResolver);
+//
 //        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//        viewResolver.setTemplateEngine(templateEngine());
+//        viewResolver.setTemplateEngine(engine);
 //        return viewResolver;
 //    }
-
-    private ClassLoaderTemplateResolver thymeleafTemplateResolver() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML");
-        templateResolver.setCharacterEncoding("UTF-8");
-        return templateResolver;
-    }
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp("/WEB-INF/views/", ".jsp");
-    }
-
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/")
-                .addResourceLocations("/resources/static/")
-                .addResourceLocations("/resources/static/css/")
-                .addResourceLocations("/resources/static/pictures/")
-                .addResourceLocations("/resources/templates/")
-                .addResourceLocations("/resources/templates/home/");
-    }
-    @Bean
-    public ViewResolver viewResolver() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setTemplateMode("XHTML");
-        templateResolver.setPrefix("views/");
-        templateResolver.setSuffix(".html");
-
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver);
-
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(engine);
-        return viewResolver;
-    }
 
 }
